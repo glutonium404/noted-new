d0dd9336c6757040ad1423503e299af6fa473d17
glutonium69 <tahzib404@gmail.com>
2026-08-05 02:39:21 +0600

    Added: api/health route

---
+++ backend/src/main/java/com/noted/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.noted.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public Map<String, String> healthCheck() {
+        return Map.of("msg", "good");
+    }
+}
